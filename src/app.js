7dece9ac406549397731f06b3d30e66a2ac4fd01
Andleus <80576769+andleus@users.noreply.github.com>
2023-11-10 18:04:36 +0000

    RandomCard fix...

--- src/app.js
+++ src/app.js
@@ -14,21 +14,22 @@ window.onload = () => {
     <div class="row number">${randomCard.number}</div>
     <div class="row symbol bottom fa-rotate-180"">${randomCard.symbol}</div>
   `;
-
 };
 
-function newRandomCard(){
-
-  let symbols = ['<i class="fa-solid fa-heart fa-2xl" style="color: #ff0000;"></i>', '<i class="fa-solid fa-diamond fa-2xl"></i>', '<i class="fa-brands fa-space-awesome fa-2xl"></i>', '<i class="fa-solid fa-spa fa-2xl"></i>'];
+function newRandomCard() {
+  let symbols = [
+    '<i class="fa-solid fa-heart fa-2xl" style="color: #ff0000;"></i>',
+    '<i class="fa-solid fa-diamond fa-2xl"></i>',
+    '<i class="fa-brands fa-space-awesome fa-2xl"></i>',
+    '<i class="fa-solid fa-spa fa-2xl"></i>'
+  ];
   let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
-
 
   const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
   const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
-  return{
+  return {
     symbol: randomSymbol,
     number: randomNumber
   };
-
-};
+}
